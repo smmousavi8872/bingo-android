2378a07053adcb650600b42c21d78f03246b3e86
Mohsen Mousavi <s.m.mousavi1993@gmail.com>
2025-10-27 20:41:19 +0330

    refactor(core:network): CardsApi renamed to CardsApiService

--- data/cards/datasource/src/main/java/com/smmousavi/developer/lvtgames/data/cards/datasource/remote/CardsRemoteDataSource.kt
+++ data/cards/datasource/src/main/java/com/smmousavi/developer/lvtgames/data/cards/datasource/remote/CardsRemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.smmousavi.developer.lvtgames.data.cards.datasource.remote
 
-import com.smmousavi.developer.lvtgames.core.model.network.CardsDto
+import com.smmousavi.developer.lvtgames.core.model.network.dto.CardsDto
+import kotlinx.serialization.InternalSerializationApi
 
+@OptIn(InternalSerializationApi::class)
 interface CardsRemoteDataSource {
     suspend fun fetchCards(): Result<CardsDto>
 }
