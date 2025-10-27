0ed586bec7c6b8544f1bee2a441dca877a87a4d9
Mohsen Mousavi <s.m.mousavi1993@gmail.com>
2025-10-27 12:14:40 +0330

    feat: Serializable models added.

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -35,10 +36,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.bundles.androidx.core)
+    implementation(libs.bundles.network.core)
+    testImplementation(libs.bundles.network.test)
 }
