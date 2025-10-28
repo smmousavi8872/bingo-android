08e2ca026402a460ae67da351a28fe243bc34ada
Mohsen Mousavi <s.m.mousavi1993@gmail.com>
2025-10-29 01:01:08 +0330

    feat(feature): splash feature module added.

--- feature/splash/build.gradle.kts
+++ feature/splash/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.smmousavi.developer.lvtgames.feature.game"
+    namespace = "com.smmousavi.developer.lvtgames.feature.splash"
     compileSdk {
         version = release(36)
     }
@@ -30,28 +29,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        compose = true
-    }
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-    api(projects.core.designsystem)
-    api(projects.domain.game.usecase)
-
-    implementation(libs.bundles.di.koin)
-
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
